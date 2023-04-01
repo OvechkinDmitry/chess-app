8f49a325914e2d41c7570bf8141bd44cfac8703d
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-04-02 01:58:42 +0500

    made common movement logic

--- chess-app/src/components/board-component/board-component.tsx
+++ chess-app/src/components/board-component/board-component.tsx
@@ -1,7 +1,8 @@
-import React, { FC, Fragment } from 'react'
+import React, { FC, Fragment, useEffect, useState } from 'react'
 import styles from './board.module.scss'
 import { Board } from '../../model/board'
 import { CellComponent } from '../cell-component/cell-component'
+import { Cell } from '../../model/cell'
 
 type TBoardComponent = {
 	board: Board
@@ -9,13 +10,46 @@ type TBoardComponent = {
 }
 
 export const BoardComponent: FC<TBoardComponent> = ({ board, setBoard }) => {
+	const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+	const onClick = (cell: Cell) => {
+		if (
+			selectedCell &&
+			selectedCell !== cell &&
+			selectedCell.figure?.canMove(cell)
+		) {
+			selectedCell.moveFigure(cell)
+			setSelectedCell(null)
+		} else setSelectedCell(cell)
+	}
+
+	useEffect(() => {
+		console.log(selectedCell)
+		highlightCells()
+	}, [selectedCell])
+
+	const updateBoard = () => {
+		const newBoard = board.getCopyBoard()
+		setBoard(newBoard)
+	}
+
+	const highlightCells = () => {
+		board.highlightCells(selectedCell)
+		updateBoard()
+	}
 	return (
 		<div className={styles.board}>
 			{board.cells.map((row, index) => {
 				return (
 					<Fragment key={index}>
 						{row.map(cell => (
-							<CellComponent cell={cell} key={cell.id}></CellComponent>
+							<CellComponent
+								click={onClick}
+								cell={cell}
+								key={cell.id}
+								selected={
+									cell.x === selectedCell?.x && cell.y === selectedCell?.y
+								}
+							></CellComponent>
 						))}
 					</Fragment>
 				)
--- chess-app/src/components/cell-component/cell-component.tsx
+++ chess-app/src/components/cell-component/cell-component.tsx
@@ -4,11 +4,26 @@ import { Cell } from '../../model/cell'
 
 type TCellComponent = {
 	cell: Cell
+	selected: boolean
+	click: (cell: Cell) => void
 }
 
-export const CellComponent: FC<TCellComponent> = ({ cell }) => {
+const getColor = (cell: Cell, selected: boolean) => {
+	if (selected && cell.figure) return 'selected'
+	else if (cell.available) return 'available'
+	else return cell.color
+}
+
+export const CellComponent: FC<TCellComponent> = ({
+	cell,
+	selected,
+	click
+}) => {
 	return (
-		<div className={['cell', cell.color].join(' ')}>
+		<div
+			className={['cell', getColor(cell, selected)].join(' ')}
+			onClick={() => click(cell)}
+		>
 			{cell.figure?.logo && <img src={cell.figure.logo} alt={''} />}
 		</div>
 	)
